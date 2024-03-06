1cdf71717287cce4e12dac0b48261bec1412b643
Стас Киселевич <kiselevich.stasik@gmail.com>
2024-03-06 22:45:03 +0300

    feat: asfsafsячСячСЯСяС

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 // Это ваш файл main.ts
 const Petya22 = 'zaza';
-
-console.log(Petya2222);
-
-console.log(pert);
-
-// Добавим комментарий или новую строку кода
-// Например:
 console.log('Это комментарий для примера');
